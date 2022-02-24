0d8ffb5b7c598f7e9487df96fe7cdea828028e35
Hao.Wu <7923324+ck_567@user.noreply.gitee.com>
2022-02-24 19:13:18 +0800

    消息编解码 分发

--- src/main/kotlin/com/ck567/netty/chatroom/util/OptionType.kt
+++ src/main/kotlin/com/ck567/netty/chatroom/util/OptionType.kt
@@ -1,26 +1,30 @@
 package com.ck567.netty.chatroom.util
 
+import com.ck567.netty.chatroom.message.HeartBeatRequestMessage
+import com.ck567.netty.chatroom.message.LoginRequestMessage
+
 enum class OptionType(
     val type: Short
 ) {
-    HeartBeatsReq(1),
-    LoginReq(2),
-    LoginRes(3)
+    LoginReq(1),
+    LoginRes(2),
+    LoginReq2(3),
+    HeartBeatReq(1793),
     ;
 
-
     companion object {
-        const val short1: Short = 1
-        const val short2: Short = 2
-        const val short3: Short = 3
-        fun getType(type:Short): OptionType{
+
+        fun getType(type:Short): Any {
             return when(type) {
-                short1 -> {
-                    HeartBeatsReq
+                LoginReq.type -> {
+                    LoginRequestMessage.serializer()
                 }
-                short2 -> LoginReq
-                short3 -> LoginRes
-                else -> HeartBeatsReq
+                LoginRes.type -> LoginRequestMessage.serializer()
+                LoginReq2.type -> LoginRequestMessage.serializer()
+                HeartBeatReq.type -> HeartBeatRequestMessage.serializer()
+                else -> {
+                    HeartBeatRequestMessage.serializer()
+                }
             }
         }
     }
