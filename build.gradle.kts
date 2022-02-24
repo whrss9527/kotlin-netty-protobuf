f865b93af27feff17522cc6151bc3c38d995b83d
Hao.Wu <7923324+ck_567@user.noreply.gitee.com>
2022-02-24 19:08:04 +0800

    增加kotlinx.serialization，用以de/en code protobuf

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.5.20"
     kotlin("plugin.spring") version "1.5.20"
+    kotlin("plugin.serialization") version "1.5.20"
 }
 
 group = "com.ck567.netty"
@@ -23,19 +24,23 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    implementation("org.projectlombok:lombok:1.18.18")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     implementation("io.netty:netty-all:4.1.63.Final")
     implementation("com.google.code.gson:gson:2.8.6")
     implementation("com.google.guava:guava:30.1.1-jre")
-    implementation("com.google.protobuf:protobuf-java:3.19.1")
-    implementation("com.google.protobuf:protoc:3.19.1")
+
     implementation("com.google.guava:guava:31.0.1-jre")
     implementation("io.netty:netty-all")
     implementation("org.springframework.boot:spring-boot-configuration-processor")
+    // protobuf
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:1.3.0")
     implementation("com.google.protobuf:protobuf-kotlin-lite:3.19.1")
-
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
+    implementation("com.google.protobuf:protobuf-java:3.19.1")
+//    implementation("com.google.protobuf:protoc:3.19.1")
+//    implementation("org.jetbrains.kotlin:kotlin-serialization:1.5.20")
+//    implementation("com.google.protobuf:protobuf-gradle-plugin:0.8.8")
 }
 
 tasks.withType<KotlinCompile> {
