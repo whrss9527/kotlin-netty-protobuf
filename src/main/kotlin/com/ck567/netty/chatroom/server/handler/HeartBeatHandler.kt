1071bdc8b4d8353dc843585e396b84751d931670
Hao.Wu <7923324+ck_567@user.noreply.gitee.com>
2022-04-29 14:39:55 +0800

    add heartbeat response

--- src/main/kotlin/com/ck567/netty/chatroom/server/handler/HeartBeatHandler.kt
+++ src/main/kotlin/com/ck567/netty/chatroom/server/handler/HeartBeatHandler.kt
@@ -1,6 +1,10 @@
 package com.ck567.netty.chatroom.server.handler
 
 import com.ck567.netty.chatroom.message.HeartBeatRequestMessage
+import com.ck567.netty.chatroom.message.HeartBeatResponseMessage
+import com.ck567.netty.chatroom.message.LoginRequestMessage
+import com.ck567.netty.chatroom.message.Message
+import com.ck567.netty.chatroom.util.OperateType
 import io.netty.channel.ChannelHandler
 import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.SimpleChannelInboundHandler
@@ -10,6 +14,7 @@ import io.netty.channel.SimpleChannelInboundHandler
 class HeartBeatHandler : SimpleChannelInboundHandler<HeartBeatRequestMessage>() {
     override fun channelRead0(ctx: ChannelHandlerContext, msg: HeartBeatRequestMessage) {
         println("收到心跳消息")
+        ctx.writeAndFlush(Message(OperateType.HeartBeatRes.type, HeartBeatResponseMessage()))
     }
 
 }
