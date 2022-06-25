5508789079f053cec58234d4c8d30d715e4c90cc
Hao.Wu <7923324+ck_567@user.noreply.gitee.com>
2022-06-25 14:43:16 +0800

    add log&jwt dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,10 +15,22 @@ java.sourceCompatibility = JavaVersion.VERSION_1_8
 repositories {
     mavenCentral()
 }
-
+configurations.all {
+    // 排除spring boot中的logging。使用log子项目。
+    exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
+    // check for updates every build
+    resolutionStrategy.cacheChangingModulesFor(0, TimeUnit.SECONDS)
+}
 dependencies {
+    val log4j2Version = "2.17.0"
+    val sprintJwtVersion = "1.0.10.RELEASE"
+    // jwt
+    implementation("org.springframework.security:spring-security-jwt:$sprintJwtVersion")
+    implementation("io.jsonwebtoken:jjwt:0.9.1")
     implementation("org.springframework.boot:spring-boot-starter-rsocket")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web"){
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
+    }
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -41,6 +53,16 @@ dependencies {
 //    implementation("com.google.protobuf:protoc:3.19.1")
 //    implementation("org.jetbrains.kotlin:kotlin-serialization:1.5.20")
 //    implementation("com.google.protobuf:protobuf-gradle-plugin:0.8.8")
+    // log
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4j2Version")
+    implementation("org.apache.logging.log4j:log4j-core:$log4j2Version") {
+        //exclusions for avoiding conflict.
+        exclude(group = "ch.qos.logback", module = "logback-classic")
+        exclude(group = "ch.qos.logback", module = "log4j-to-slf4j")
+    }
+    implementation("org.apache.logging.log4j:log4j-api:$log4j2Version")
+    implementation("org.apache.logging.log4j:log4j-jul:$log4j2Version")
+    implementation("org.slf4j:jul-to-slf4j:1.7.25")
 }
 
 tasks.withType<KotlinCompile> {
