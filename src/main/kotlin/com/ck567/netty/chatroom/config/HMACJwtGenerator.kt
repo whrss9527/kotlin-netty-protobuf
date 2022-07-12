3150629a0e6355d6479f0b5fab5c30dd7be9b48f
Hao.Wu <7923324+ck_567@user.noreply.gitee.com>
2022-07-12 14:17:05 +0800

    delete generate

--- src/main/kotlin/com/ck567/netty/chatroom/config/HMACJwtGenerator.kt
+++ src/main/kotlin/com/ck567/netty/chatroom/config/HMACJwtGenerator.kt
@@ -1,39 +1,16 @@
 package com.ck567.netty.chatroom.config
 
-import com.ck567.netty.chatroom.util.loggerFrom
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
-import java.util.*
+
 
 class HMACJwtGenerator(
-    private val issuer: String,
-    private val expiredMinutes: Int,
     private val accessTokenSecret: String
-): JwtValidator {
-
-    /**
-     * 支持多种audience
-     */
-    fun generate(audience: String, subject: String, secret: String, authorities: Set<String> = emptySet()): String {
-
-        val builder = Jwts.builder()
-            .setIssuer(issuer)
-            .setAudience(audience)
-            .setSubject(subject)
-            .setExpiration(Date(System.currentTimeMillis() + expiredMinutes * 60 * 1000))
-            .signWith(SignatureAlgorithm.HS512, secret)
-        if (authorities.isNotEmpty()){
-            builder.claim("authorities", authorities.joinToString(","))
-        }
-        return builder.compact()
-    }
+) : JwtValidator {
 
     override fun validate(token: String): Claims {
 
         return Jwts.parser().setSigningKey(accessTokenSecret)
-            .parseClaimsJws(token.replace("Bearer ", "")
-                .also { loggerFrom(this).debug("accessToken=\n$it")  }
-            ).body
+            .parseClaimsJws(token.replace("Bearer ", "")).body
     }
 }
