e1f0e5216606802043c0110f32ec29a2f8396783
Hao.Wu <7923324+ck_567@user.noreply.gitee.com>
2022-07-12 18:10:57 +0800

    no heart, close connect.

--- src/main/kotlin/com/ck567/netty/chatroom/server/handler/ServerIdleStateHandler.kt
+++ src/main/kotlin/com/ck567/netty/chatroom/server/handler/ServerIdleStateHandler.kt
@@ -1,5 +1,8 @@
 package com.ck567.netty.chatroom.server.handler
 
+import com.ck567.netty.chatroom.server.message_bus.Consumer
+import com.ck567.netty.chatroom.server.session.SessionFactory
+import com.ck567.netty.chatroom.util.logger
 import io.netty.channel.ChannelHandler
 import io.netty.channel.ChannelHandlerContext
 import io.netty.handler.timeout.IdleStateEvent
@@ -12,7 +15,8 @@ import java.util.concurrent.TimeUnit
 class ServerIdleStateHandler :
     IdleStateHandler(READER_IDLE_TIME.toLong(), 0, 0, TimeUnit.SECONDS) {
     override fun channelIdle(ctx: ChannelHandlerContext, evt: IdleStateEvent) {
-        println("{} 秒内没有读取到数据,关闭连接$READER_IDLE_TIME")
+        logger.debug("{} 秒内没有读取到数据,关闭连接", READER_IDLE_TIME)
+        SessionFactory.getSession().unbind(ctx.channel())
         ctx.channel().close()
     }
 
@@ -21,5 +25,6 @@ class ServerIdleStateHandler :
          * 空闲检测时间单位：秒
          */
         private const val READER_IDLE_TIME = 15
+        private val logger = logger<ServerIdleStateHandler>()
     }
 }
