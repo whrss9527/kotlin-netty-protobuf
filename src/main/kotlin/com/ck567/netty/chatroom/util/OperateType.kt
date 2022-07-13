5620647e97899da51d7fc3c4c139c31360a62c4e
Hao.Wu <7923324+ck_567@user.noreply.gitee.com>
2022-07-13 10:43:57 +0800

    Connection establishment and login at the same time

--- src/main/kotlin/com/ck567/netty/chatroom/util/OperateType.kt
+++ src/main/kotlin/com/ck567/netty/chatroom/util/OperateType.kt
@@ -2,15 +2,11 @@ package com.ck567.netty.chatroom.util
 
 import com.ck567.netty.chatroom.message.HeartBeatRequestMessage
 import com.ck567.netty.chatroom.message.HeartBeatResponseMessage
-import com.ck567.netty.chatroom.message.LoginRequestMessage
 
 enum class OperateType(
     val type: Short
 ) {
     // 搞几个枚举放在这里
-    LoginReq(1),
-    LoginRes(2),
-    LoginReq2(3),
     HeartBeatReq(1793),
     HeartBeatRes(1795),
     ;
@@ -20,11 +16,6 @@ enum class OperateType(
         fun getSerializer(type:Short): Any {
             // 拿对应的序列化对象
             return when(type) {
-                LoginReq.type -> {
-                    LoginRequestMessage.serializer()
-                }
-                LoginRes.type -> LoginRequestMessage.serializer()
-                LoginReq2.type -> LoginRequestMessage.serializer()
                 HeartBeatReq.type -> HeartBeatRequestMessage.serializer()
                 HeartBeatRes.type -> HeartBeatResponseMessage.serializer()
                 else -> {
